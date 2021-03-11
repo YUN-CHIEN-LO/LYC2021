5d125297a64ec4a1ec3736d9820d05a60d7c4ead
Yun-Chien Lo <yunchienlo@gmail.com>
2021-03-12 00:39:52 +0800

    typo

--- js/vue.js
+++ js/vue.js
@@ -214,7 +214,7 @@ var vm_eng = new Vue({
 var event_data = [{
         date: 2020,
         company: ["Programming Project", "table"],
-        position: "Boostrap Table Practice",
+        position: "Bootstrap Table Practice",
         url: "https://i.imgur.com/2Hfz2Xwl.png",
         link: "https://yun-chien-lo.github.io/demo/index.html"
     }, {
